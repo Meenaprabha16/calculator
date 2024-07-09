735a6e8cf28ded2cf02dca56f86617c9b48a27b2
Meenaprabha16 <145092466+Meenaprabha16@users.noreply.github.com>
2024-07-09 15:11:32 +0530

    Add files via upload

---
+++ calculator/calculator.js
@@ -0,0 +1,13 @@
+function displayvalue(val){
+    document.getElementById("display").value = document.getElementById("display").value + val;
+}
+function clearDisplay(){
+    document.getElementById("display").value = "";
+}
+function calculate('='){
+   var userInput = document.getElementById("dispaly").value;
+   var result = eval(userInput);
+   
+   document.getElementById("display").value = result;
+
+}
